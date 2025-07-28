8aeef11cfe69e4c0a4faf81296f53edb2acb00cd
Tamim Hossain <amdtamim3@gmail.com>
2025-07-29 03:06:59 +0600

    Change environment variable from PORT to APP_PORT

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Server Configuration
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.APP_PORT || 3000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     
     // Database Configuration
